f0acb2ff444005746b0770e67ab7749b23202725
BHASHITHA <Pramudith.bhashitha17@gmail.com>
2021-04-22 18:32:07 +0530

    Update script.py

--- script.py
+++ script.py
@@ -3,18 +3,19 @@ class script(object):
 
 <b>Send me any Telegram file and choose appropriate option! </b>"""
 
-    RENAME_403_ERR = "What Are You Doing? You are Banned"
-    UPGRADE_TEXT = "CONTACT @prgofficial"
-    DOWNLOAD_START = "Give Me Some Time..."
-    UPLOAD_START = "Starting to upload..."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me > ©  @prgofficial **"
-    SAVED_THUMB = "Thumbnail Saved ✅ This Is Permanent"
-    DEL_THUMB = "Thumbnail cleared succesfully!"
-    NO_THUMB = "No thumbnails found!"
-    SAVED_RECVD_DOC_FILE = "File Downloaded Successfully 😎"
+    RENAME_403_ERR = "**What Are You Doing? You are Banned 😂**"
+    UPGRADE_TEXT = "**❄CONTACT @Network_hacker_bots **"
+    DOWNLOAD_START = "**Give Me Some Time...**"
+    UPLOAD_START = "**⚡Starting to upload...⚡**"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me > ©  @Network_hacker_bots 🇱🇰 **"
+    SAVED_THUMB = "**Thumbnail Saved ✅ This Is Permanent**"
+    DEL_THUMB = "**Thumbnail cleared succesfully!**"
+    NO_THUMB = "**No thumbnails found!🤒**"
+    SAVED_RECVD_DOC_FILE = "**File Downloaded Successfully 😎**"
     CUSTOM_CAPTION_UL_FILE = " "
-    HELP_USER = """It's not that complicated😅
+    HELP_USER = """**It's not that complicated😅
     
 1. Send me any Telegram File.
-2. Choose appropriate option."""
+2. Choose appropriate option.
+   ©Network_hacker_bots 🇱🇰 **"""
 
