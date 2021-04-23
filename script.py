7bbd752a6fa16abc4a66b4052a827a4bac786790
BHASHITHA <Pramudith.bhashitha17@gmail.com>
2021-04-23 22:15:19 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,11 +1,11 @@
 class script(object):
     START_TEXT = """ A Simple File Renamer Bot With Permanent Thumbnail support!💯
 
-<b>Send me any Telegram file and choose appropriate option! </b>"""
+<b>Send me /help any Telegram file and choose appropriate option! </b>"""
 
     RENAME_403_ERR = "**What Are You Doing? You are Banned 😂**"
-    UPGRADE_TEXT = "**❄CONTACT @Network_hacker_bots **"
-    DOWNLOAD_START = "**Give Me Some Time...**"
+    UPGRADE_TEXT = "🌟 CONTACT @Network_hacker_bots"
+    DOWNLOAD_START = "**🌟 Give Me Some Time... 🌟**"
     UPLOAD_START = "**⚡Starting to upload...⚡**"
     AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me > ©  @Network_hacker_bots 🇱🇰 **"
     SAVED_THUMB = "**Thumbnail Saved ✅ This Is Permanent**"
@@ -13,9 +13,9 @@ class script(object):
     NO_THUMB = "**No thumbnails found!🤒**"
     SAVED_RECVD_DOC_FILE = "**File Downloaded Successfully 😎**"
     CUSTOM_CAPTION_UL_FILE = " "
-    HELP_USER = """**It's not that complicated😅
+    HELP_USER = """It's not that complicated😅
     
 1. Send me any Telegram File.
 2. Choose appropriate option.
-   ©Network_hacker_bots 🇱🇰 **"""
+   ©Network_hacker_bots 🇱🇰 """
 
